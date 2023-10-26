dfeb813770e563e2372468f28409cd85afcce25b
Saptak <142655066+Saptakbro@users.noreply.github.com>
2023-10-26 15:18:05 +0600

    Update ERC-20 token.sol

--- contracts/ERC-20 token.sol
+++ contracts/ERC-20 token.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract TokenContract is ERC20, ERC20Mintable, ERC20Burnable {
+    constructor(string memory name, string memory symbol)
+        ERC20(name, symbol)
+    {}
+}
 
