934372313fa7bc1dd3c9242978807e47566c3a46
Saptak <142655066+Saptakbro@users.noreply.github.com>
2023-11-05 13:23:39 +0600

    App.js

---
+++ test/src_App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import "./App.css";
+import WalletBalance from "./components/WalletBalance";
+
+function App() {
+ return (
+    <div className="App">
+      <WalletBalance />
+    </div>
+ );
+}
+
+export default App;
